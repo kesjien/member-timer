e2dd03366a1ba830c66bf769a400bdb1773ab1b7
Glib Kechyn <glkes@yahoo.com>
2017-06-30 13:42:57 +0300

    stop start feature

--- components/main.js
+++ components/main.js
@@ -5,8 +5,11 @@ import BackgroundTimer from 'react-native-background-timer';
 export default class Main extends React.Component {
   constructor() {
     super();
+    this.intervalId = 0;
+    this.diff = 0;
     this.startTimer = this.startTimer.bind(this);
     this.state = {
+      diff: 30,
       timerMinutes: '00',
       timerSeconds: '00',
       startAnimation: false,
@@ -14,15 +17,19 @@ export default class Main extends React.Component {
   }
 
   startTimer() {
-    this.setState({
-      startAnimation: true
-    })
-    let diff, minutes, seconds, start = Date.now();
-    const intervalId = BackgroundTimer.setInterval(() => {
-      diff = 30 - (((Date.now() - start) / 1000) | 0);
-
-      minutes = (diff / 60) | 0;
-      seconds = (diff % 60) | 0;
+    if (this.state.startAnimation && this.intervalId){
+      this.setState({diff: this.diff})
+      BackgroundTimer.clearInterval(this.intervalId);
+      this.setState({
+        startAnimation: false
+      })
+      return;
+    }
+    let minutes, seconds, start = Date.now();
+    this.intervalId = BackgroundTimer.setInterval(() => {
+      this.diff = this.state.diff - (((new Date() - start) / 1000) | 0);
+      minutes = (this.diff / 60) | 0;
+      seconds = (this.diff % 60) | 0;
 
       minutes = minutes < 10 ? "0" + minutes : minutes;
       seconds = seconds < 10 ? "0" + seconds : seconds;
@@ -32,15 +39,19 @@ export default class Main extends React.Component {
         timerSeconds: seconds
       })
 
-      if (diff <= 0) {
-        start = Date.now() + 1000;
-        BackgroundTimer.clearInterval(intervalId);
+      if (this.diff <= 0) {
+        this.startInterval = Date.now() + 1000;
+        BackgroundTimer.clearInterval(this.intervalId);
         this.setState({
           startAnimation: false
         })
         Vibration.vibrate([500, 500, 500, 500, 500, 500]);
       }
     }, 1000);
+
+    this.setState({
+      startAnimation: true
+    })
   }
 
   render() {
